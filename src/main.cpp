53ab48f328ad8665e27ee07a078e7496d12e9462
Marvin Reza <marvinreza@outlook.com>
2018-04-19 19:30:08 +0200

    Edited main function for cluster generator

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,7 @@
 #include "cluster_generator.h"
 
 typedef struct {
-    float r, g, b;
+    float r, g, b, a;
 } color;
 
 void draw_line(float x1, float y1, float x2, float y2) 
@@ -50,8 +50,8 @@ int main(int, char **)
         {
             float x = data.vals[i][0];
             float y = data.vals[i][1];
-            glColor4f(0.3f, 0.7f, 0.3f, 1.0f);
 
+            glColor4f(0.3f, 0.7f, 0.3f, 1.0f);
             glPoints(8.0f);
             glVertex2f(x, y);
             glEnd();
@@ -81,38 +81,54 @@ int main(int, char **)
 
         TextWrapped("This shows an example of how k-means clustering works.");
 
-    if (ImGui::Button("Load..")) ImGui::OpenPopup("Load data from CSV?");
-    if (ImGui::BeginPopupModal("Load data from CSV?", NULL, ImGuiWindowFlags_AlwaysAutoResize))
-    {
-        static char filename[1024];
-        static bool init_filename = true;
-        ImGui::InputText("Filename", filename, sizeof(filename));
-        ImGui::Separator();
-
-        if (ImGui::Button("OK", ImVec2(120,0))) 
+        if (ImGui::Button("Load..")) ImGui::OpenPopup("Load data from CSV?");
+        if (ImGui::BeginPopupModal("Load data from CSV?", NULL, ImGuiWindowFlags_AlwaysAutoResize))
         {
-            data = csv_to_matrix(filename);
-            ImGui::CloseCurrentPopup();
+            static char filename[1024];
+            static bool init_filename = true;
+            ImGui::InputText("Filename", filename, sizeof(filename));
+            ImGui::Separator();
+
+            if (ImGui::Button("OK", ImVec2(120,0))) 
+            {
+                data = csv_to_matrix(filename);
+                ImGui::CloseCurrentPopup();
+            }
+            ImGui::SameLine();
+            if (ImGui::Button("Cancel", ImVec2(120,0)))
+            {
+                ImGui::CloseCurrentPopup();
+            }
+            ImGui::EndPopup();
         }
+
         ImGui::SameLine();
-        if (ImGui::Button("Cancel", ImVec2(120,0)))
+        if (ImGui::Button("Random"))
         {
-            ImGui::CloseCurrentPopup();
+            data = create_random_matrix(rng0.Rand(500), cols);
         }
-        ImGui::EndPopup();
-    }
 
-    ImGui::SameLine();
-    if (ImGui::Button("Random"))
-    {
-        data = create_random_matrix(rows, cols);
-    }
+        if (ImGui::CollapsingHeader("Generate clusters"))
+        {
 
-    if(ImGui::Button("Cluster"))
-    {
-        std::vector<std::vector<float> > centroids;
-    }
+            static int num_clusters = 0;
+            ImGui::SliderInt("number of clusters", &num_clusters, 0, 100);
+            ImGui::SameLine(); ShowHelpMarker("CTRL+click to input value.");
 
+            static float sigma = 0.1f;
+            ImGui::SliderFloat("sigma", &sigma, 0.0f, 1.0f, "sigma = %.3f");
+            ImGui::SameLine(); ShowHelpMarker("CTRL+click to input value.");
+
+            if(ImGui::Button("Generate"))
+            {
+                matrix_t centroids = create_random_matrix(num_clusters, 2);
+                auto clusters = generate_clusters(centroids, 50, sigma);
+                clear_matrix(&data);
+                for(const auto& cluster : clusters) {
+                    data = concat_matrix(data, cluster);
+                }
+            }
+        }
     }
     VDBE();
 
