d1138a47d822aa0b4cf079bf6936fa8893e455bb
Marvin Reza <marvinreza@outlook.com>
2018-05-20 14:26:54 +0200

    avoid negative numbers using iou as metric

--- src/kmeans.cpp
+++ src/kmeans.cpp
@@ -60,8 +60,7 @@ std::pair<int, float> get_closest_center(const std::vector<float>& data, const m
 {
     int closest_center = 0;
     float closest_dist = dist(data, centers.vals[closest_center], metric);
-    for(int i = 0; i < centers.rows; ++i)
-    {
+    for(int i = 0; i < centers.rows; ++i) {
         float cur_dist = dist(data, centers.vals[i], metric);
         if(cur_dist < closest_dist) {
             closest_dist = cur_dist;
@@ -74,8 +73,7 @@ std::pair<int, float> get_closest_center(const std::vector<float>& data, const m
 bool kmeans_expectation(matrix_t data, model_t* model, kmeans_metric_t metric)
 {
     bool converged = true;
-    for(int i = 0; i < data.rows; ++i) 
-    {
+    for(int i = 0; i < data.rows; ++i) {
         auto closest = get_closest_center(data.vals[i], model->centers, metric);
         float closest_center_idx = closest.first;
         if(closest_center_idx != model->assignments[i]) converged = false;
@@ -105,6 +103,13 @@ void kmeans_maximization(matrix_t data, model_t* model)
 
 model_t kmeans(matrix_t data, int k, kmeans_metric_t metric, bool use_smart_centers)
 {
+    if(metric == IOU) {
+        for(int i = 0; i < data.rows; ++i) {
+            for(int j = 0 ; j < data.cols; ++j) 
+                assert(data.vals[i][j] > 0);
+        }
+    }
+
     matrix_t centers = make_matrix(k, data.cols);
     std::vector<int> assignments(data.rows, 0);
     model_t model = {assignments, centers};
