46a114711f162df271f313e9acbceca497b7a9e6
Marvin Reza <marvinreza@outlook.com>
2018-06-13 11:06:16 +0200

    implemented save and load functions for images and simple image operations

--- include/image.h
+++ include/image.h
@@ -10,12 +10,27 @@ typedef struct {
 } image_t;
 
 image_t make_image(int w, int h, int c);
+image_t make_image_from_chw_bytes(int w, int h, int c, unsigned char* data);
+image_t make_image_from_hwc_bytes(int w, int h, int c, unsigned char* data);
+
+image_t copy_image(const image_t& m);
+void copy_image(const image_t& src, image_t* dst);
 void clear_image(image_t* m);
 
-void set_pixel(image_t* m, int x, int y, int c, float val);
+void scale_image(image_t* m, float s);
+void translate_image(image_t* m, float s);
+void fill_image(image_t* m, float s);
 
 void draw_box(image_t* m, int x1, int y1, int x2, int y2, float r, float g, float b);
 
+// Get HWC(channels interleaved) bytes from CHW(channels separate) float image
+std::vector<unsigned char> get_hwc_bytes(const image_t& m);
+
+image_t load_image(const char* filename, int num_channels = 3);
+image_t load_image_rgb(const char* filename);
+image_t load_image_grayscale(const char* filename);
+
 void save_image_png(const image_t& m, const char* filename);
+void save_image_jpg(const image_t& m, const char* filename, int quality = 100);
 
 #endif
--- src/image.cpp
+++ src/image.cpp
@@ -1,20 +1,88 @@
 #include "image.h"
 
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb_image_write.h"
+
 image_t make_image(int w, int h, int c)
 {
     image_t out = {w, h, c, std::vector<float>(w*h*c, 0.f)};
     return out;
 }
 
-void clear_image(image_t* m)
+image_t make_image_from_chw_bytes(int w, int h, int c, unsigned char* data)
 {
-    m->data = std::vector<float>(m->w*m->h*m->c, 0);
+    image_t out = make_image(w,h,c);
+    for(int i = 0; i < w*h*c; ++i) {
+        out.data[i] = (float)data[i] / 255.f;
+    }
+    return out;
 }
 
-void set_pixel(image_t* m, int x, int y, int c, float val)
+image_t make_image_from_hwc_bytes(int w, int h, int c, unsigned char* data)
+{
+    image_t m = make_image(w, h, c);
+    for(int k = 0; k < c; ++k) {
+        for(int j = 0; j < h; ++j) {
+            for(int i = 0; i < w; ++i) {
+                m.data[i + w*j + w*h*k] = (float)data[k + c*i + c*w*j]/255.f;
+            }
+        }
+    }
+    return m;
+}
+
+void clear_image(image_t* m)
+{
+    m->data = std::vector<float>(m->w*m->h*m->c, 0.f);
+}
+
+void scale_image(image_t* m, float s)
+{
+    for(int i = 0; i < m->h*m->w*m->c; ++i) 
+        m->data[i] *= s;
+}
+
+void translate_image(image_t* m, float s)
+{
+    for(int i = 0; i < m->h*m->w*m->c; ++i) 
+        m->data[i] += s;
+}
+
+void fill_image(image_t* m, float s)
+{
+    for(int i = 0; i < m->h*m->w*m->c; ++i)
+        m->data[i] = s;
+}
+
+void copy_image(const image_t& src, image_t* dst)
+{
+    *dst = { src.w, src.h, src.c, src.data };
+}
+
+image_t copy_image(const image_t& m)
+{
+    return {m.w, m.h, m.c, m.data};
+}
+
+static void set_pixel(image_t* m, int x, int y, int c, float val)
 {
     if (x < 0 || y < 0 || c < 0 || x >= m->w || y >= m->h || c >= m->c) return;
     m->data[(c * m->h * m->w) + (y * m->w) + x] = val;
+}
+
+static float get_pixel(const image_t& m, int x, int y, int c)
+{
+    assert(x < m.w && y < m.h && c < m.c);
+    return m.data[x + y*m.w + c*m.h*m.w];
+}
+
+static float get_pixel_extend(const image_t& m, int x, int y, int c)
+{
+    if(x < 0 || x >= m.w || y < 0 || y >= m.h) return 0;
+    if(c < 0 || c >= m.c) return 0;
+    return get_pixel(m, x, y, c);
 }
 
 void draw_box(image_t* m, int x1, int y1, int x2, int y2, float r, float g, float b)
@@ -46,4 +114,57 @@ void draw_box(image_t* m, int x1, int y1, int x2, int y2, float r, float g, floa
         set_pixel(m, x1, i, 2, b);
         set_pixel(m, x2, i, 2, b);
     }
+}
+
+std::vector<unsigned char> get_hwc_bytes(const image_t& m)
+{
+    std::vector<unsigned char> bytes(m.c*m.h*m.w, 0);
+    for(int k = 0; k < m.c; ++k) {
+        for(int i = 0; i < m.w*m.h; ++i) {
+            bytes[i*m.c+k] = (unsigned char) (255*m.data[i + k*m.w*m.h]);
+        }
+    }
+    return bytes;
+}
+
+image_t load_image(const char* filename, int num_channels)
+{
+    int w, h, c;
+    unsigned char* data = stbi_load(filename, &w, &h, &c, num_channels);
+    if (!data) {
+        fprintf(stderr, "Cannot load image \"%s\"\nSTB Reason: %s\n", filename, stbi_failure_reason());
+        exit(0);
+    }
+    if(num_channels) c = num_channels;
+    image_t img =  make_image_from_hwc_bytes(w,h,c, data);
+    delete[] data;
+    return img;
+}
+
+image_t load_image_rgb(const char* filename)
+{
+    return load_image(filename, 3);
+}
+
+image_t load_image_grayscale(const char* filename)
+{
+    return load_image(filename, 1);
+}
+
+void save_image_png(const image_t& m, const char* filename)
+{
+    char buffer[256];
+    sprintf(buffer, "%s.png", filename);
+    std::vector<unsigned char> pixels = get_hwc_bytes(m);
+    int success = stbi_write_png(buffer, m.w, m.h, m.c, pixels.data(), m.w*m.c);
+    if(!success) fprintf(stderr, "Failed to write image %s\n", buffer);
+}
+
+void save_image_jpg(const image_t& m, const char* filename, int quality)
+{
+    char buffer[256];
+    sprintf(buffer, "%s.jpg", filename);
+    std::vector<unsigned char> pixels = get_hwc_bytes(m);
+    int success = stbi_write_jpg(buffer, m.w, m.h, m.c, pixels.data(), quality);
+    if(!success) fprintf(stderr, "Failed to write image %s\n", buffer);
 }
