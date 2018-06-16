b473e38102e8441dff33bc6261c3602562476a04
Marvin Reza <marvinreza@outlook.com>
2018-06-17 00:28:03 +0200

    new page for image processing

--- src/gui_content/image_processing.cpp
+++ src/gui_content/image_processing.cpp
@@ -1,6 +1,8 @@
 VDBB("Image processing");
 {
+    vdb2D(-1, +1, +1, -1);
     static ImGuiFs::Dialog dialog;
+    static GLenum data_format = GL_RGB;
     static image_t loaded_image = make_image(100,100,3);
     static image_t screen_image = copy_image(loaded_image);
 
@@ -10,27 +12,46 @@ VDBB("Image processing");
     if(strcmp(chosen_path, "")) {
         loaded_image = load_image_rgb(chosen_path);
         screen_image = copy_image(loaded_image);
+        save_image_png(screen_image, "lal");
     }
 
-    unsigned char* image_data = get_hwc_bytes(screen_image).data();
-    vdbSetTexture(0, image_data, screen_image.w, screen_image.h, GL_RGB);
+    std::vector<unsigned char> image_data = get_hwc_bytes(screen_image);
+    vdbSetTexture(0, image_data.data(), screen_image.w, screen_image.h, data_format, GL_UNSIGNED_BYTE);
     vdbDrawTexture(0);
 
     static float binary_threshold = 0.f, prev_binary_threshold = 0.f;
-    static float white_threshold_r, white_threshold_g, white_threshold_b, white_threshold_a;
     if (ImGui::CollapsingHeader("Binary threshold")) {
         ImGui::SliderFloat("binary_threshold", &binary_threshold, 0.0f, 255.0f);
-        if(prev_binary_threshold != binary_threshold) {
+
+        if(binary_threshold != prev_binary_threshold) {
             threshold_image(loaded_image, &screen_image, binary_threshold / 255.f);
+            data_format = GL_RGB;
         }
         prev_binary_threshold = binary_threshold;
     }
 
+    static float white_threshold_r, white_threshold_g, white_threshold_b, white_threshold_d = 255.f;
+    static float prev_threshold_r, prev_threshold_g, prev_threshold_b, prev_threshold_d = 255.f;
     if (ImGui::CollapsingHeader("White threshold")) {
         ImGui::SliderFloat("white_threshold_r", &white_threshold_r, 0.0f, 255.0f);
         ImGui::SliderFloat("white_threshold_g", &white_threshold_g, 0.0f, 255.0f);
         ImGui::SliderFloat("white_threshold_b", &white_threshold_b, 0.0f, 255.0f);
-        ImGui::SliderFloat("white_threshold_a", &white_threshold_a, 0.0f, 255.0f);
+        ImGui::SliderFloat("white_threshold_d", &white_threshold_d, 0.0f, 255.0f);
+        if(prev_threshold_r != white_threshold_r || prev_threshold_g != white_threshold_g ||
+            prev_threshold_b != white_threshold_b || prev_threshold_d != white_threshold_d)
+        {
+            threshold_image(loaded_image, 
+                            &screen_image, 
+                            white_threshold_r / 255.f, 
+                            white_threshold_g / 255.f, 
+                            white_threshold_b / 255.f, 
+                            white_threshold_d / 255.f);
+            data_format = GL_LUMINANCE;
+        }
+        prev_threshold_r = white_threshold_r;
+        prev_threshold_g = white_threshold_g;
+        prev_threshold_b = white_threshold_b;
+        prev_threshold_d = white_threshold_d;
     }
 
     static float r_g, r_b, r_n;
@@ -57,8 +78,9 @@ VDBB("Image processing");
         ImGui::SliderFloat("minimum brightness (b_n)", &b_n, 0.0f, 255.0f);
     }
 
-    if(colored_button("Reset", 0.75f)) {
+    if(colored_button("Reset", 0.f)) {
         screen_image = loaded_image;
+        data_format = GL_RGB;
     }
 }
 VDBE();
