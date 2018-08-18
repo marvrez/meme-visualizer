489176ea8dcf6ec6927c048795362486e6ba6ff6
Marvin Reza <marvinreza@outlook.com>
2018-08-19 00:54:48 +0200

    lol nvm, we dropping libsvm and making our own svm instead

--- include/svm.h
+++ include/svm.h
@@ -1,190 +1,67 @@
+/*
+    This is a binary SVM and is trained using the SMO algorithm.
+    Reference: "The Simplified SMO Algorithm" (http://math.unt.edu/~hsp0009/smo.pdf)
+*/
 #ifndef SVM_H
 #define SVM_H
-
 #include <vector>
 
-// the implementation is basically a simplification of libsvm
-// found in https://github.com/cjlin1/libsvm
+typedef enum {
+    RBF,
+    LINEAR
+} svm_kernel_type_t;
 
 typedef struct {
-    int index; // -1 = end of a vector
-    float value;
-} svm_node_t;
+    double C = 1.0f;
+    double eps = 1e-4; // numerical tolerance. Don't touch unless you're pro
+    double alpha_tol = 1e-7; // non-support vectors for space and time efficiency are truncated. To guarantee correct result set this to 0 to do no truncating. If you want to increase efficiency, experiment with setting this little higher, up to maybe 1e-4 or so.
+
+    long long max_iter = 10000; // max number of iterations
+    int num_passes = 10; // how many passes over data with no change before we halt? Increase for more precision.
+
+    bool do_cache = true; // cache kernel computations to avoid expensive recomputations. Could use too much memory if N is too large.
+
+    double gamma = 2.f; // for rbf, equivalent to 1/2*sigma^2
+
+    svm_kernel_type_t kernel_type = RBF; // Currently between RBF and LINEAR kernel
+
+    std::vector<std::vector<double> > datum;
+    std::vector<int> labels;
+
+} svm_parameter_t;
 
 typedef struct {
-	int l; // number of training data
-    std::vector<float> y; // target values
-    std::vector<std::vector<svm_node_t> > x; // training vecs
-} svm_problem_t;
+    svm_parameter_t param;
+    int N, D; // N is number of data points, D is the dimension of the data
+    std::vector<double> alpha;
+    double b = 0;
+    bool use_w = false;  // internal efficiency flag
 
-typedef enum { 
-    C_SVC, 
-    NU_SVC, 
-    ONE_CLASS, 
-    EPSILON_SVR, 
-    NU_SVR 
-} svm_type_t;
+    double (*kernel)(const std::vector<double>& v1, const std::vector<double>& v2);
 
-typedef enum { 
-    LINEAR, 
-    POLY, 
-    RBF, 
-    SIGMOID, 
-    RBFWEIGH, 
-    RBFWMATRIX, 
-    PRECOMPUTED 
-} kernel_type_t; 
-
-struct svm_parameter_t {
-	svm_type_t svm_type;
-    kernel_type_t kernel_type;
-	int degree;				/* for poly */
-	float gamma;			/* for poly/rbf/sigmoid */
-	float coef0;			/* for poly/sigmoid */
-    std::vector<float> kernel_weight;	/* for rbfweight/rbfwmatrix */
-	int kernel_dim;			/* for rbfweight/rbfwmatrix */
-	bool normalize_kernel;
-	float kernel_norm;
-
-	/* these are for training only */
-	float cache_size;		/* in MB */
-	float eps;				/* stopping criteria */
-	float C;				/* for C_SVC, EPSILON_SVR and NU_SVR */
-	int nr_weight;			/* for C_SVC */
-    std::vector<float> weight_label;		/* for C_SVC */
-    std::vector<float> weight;			/* for C_SVC */
-	float nu;				/* for NU_SVC, ONE_CLASS, and NU_SVR */
-	float p;				/* for EPSILON_SVR */
-	int shrinking;			/* use the shrinking heuristics */
-	int probability;		/* do probability estimates */
-
-    svm_parameter_t() : kernel_weight(0), weight_label(0), weight(0), kernel_dim(0), nr_weight(0) { };
-    svm_parameter_t& operator= (const svm_parameter_t& param);
-};
-
-typedef struct {
-	svm_parameter_t param;	// parameter
-	int nr_class;			// number of classes, = 2 in regression/one class svm
-	int l;					// total #SV
-    std::vector<std::vector<svm_node_t> > SV;			// SVs (SV[l])
-    std::vector<float> sv_coef;		// coefficients for SVs in decision functions (sv_coef[k-1][l])
-    std::vector<float> rho;			// constants in decision functions (rho[k*(k-1)/2])
-    std::vector<float> probA;		// pairwise probability information
-    std::vector<float> probB;
-    std::vector<float> eps;
-
-	// for classification only
-    std::vector<int> label;				// label of each class (label[k])
-    std::vector<int> nSV;				// number of SVs for each class (nSV[k])
-							// nSV[0] + nSV[1] + ... + nSV[k-1] = l
-	int free_sv;			// 1 if svm_model is created by svm_load_model
-							// 0 if svm_model is created by svm_train
+    std::vector<double> w; // cache weights for linear kernels to speed up evaluation during test time.
 } svm_model_t;
 
-// Kernel evaluation:
-// the static method k_function is for doing single kernel evaluation
-// the constructor of Kernel prepares to calculate the l*l kernel matrix
-// the member function get_Q is for getting one column from the Q Matrix
-class QMatrix {
-public:
-	virtual float* get_q(int column, int len) const = 0;
-	virtual float* get_qd() const = 0;
-	virtual void swap_index(int i, int j) const = 0;
-	virtual ~QMatrix() {}
-};
+double kernel_linear(const std::vector<double>& v1, const std::vector<double>& v2);
+double kernel_rbf(const std::vector<double>& v1, const std::vector<double>& v2);
 
-// Kernel Cache:
-// l is the number of total data items
-// size is the cache size limit in bytes
-class Cache {
-public:
-	Cache(int l, long long size);
-	~Cache() { delete[] m_head; };
+// data is NxD array of doubles. labels are 1 or -1.
+void svm_train(const svm_parameter_t& param, svm_model_t* model, int* train_iter);
+void svm_train(const std::vector<std::vector<double> >& datum, const std::vector<int>& labels);
 
-	// request data [0,len)
-	// return some position p where [p,len) need to be filled
-	// (p >= len if nothing needs to be filled)
-	int get_data(const int index, std::vector<float>* data, int len);
-	void swap_index(int i, int j);	// future_option
-private:
-	const int m_l;
-	long long m_cache_mem_size;
+// inst is an array of length D. Returns margin of given example
+// this is the core prediction function. All others are for convenience mostly and end up calling this one somehow.
+double svm_margin(const svm_model_t& model, const std::vector<double>& example);
+// data is an NxD array. Returns vector of margins.
+std::vector<double> svm_margins(const svm_model_t& model, const std::vector<std::vector<double> >& data);
 
-    // Circular LRU linked list implementation
-	struct head_t { 
-        head_t* prev, *next;	
-        int len; // data[0,len)
-        std::vector<float> data;
-	};
+// Return predictions for given example
+int svm_predict(const svm_model_t& model, const std::vector<double>& example);
+// data is an NxD matrix. Returns vector of 1 or -1, predictions
+std::vector<int> svm_predict(const svm_model_t& model, const std::vector<std::vector<double> >& data);
 
-	head_t* m_head;
-	head_t m_lru_head;
-	void lru_delete(head_t* h);
-	void lru_insert(head_t* h);
-};
-
-class Kernel: public QMatrix {
-public:
-	Kernel(int l, svm_node_t* const* x, const svm_parameter_t& param);
-	virtual ~Kernel() { delete[] m_x; };
-
-	static float k_function(const svm_node_t* x, const svm_node_t* y, const svm_parameter_t& param);
-
-	virtual float* get_q(int column, int len) const = 0;
-	virtual float* get_qd() const = 0;
-	virtual void swap_index(int i, int j);
-
-protected:
-	float (Kernel::*kernel_function)(int i, int j) const;
-
-private:
-	const svm_node_t** m_x;
-    std::vector<float> m_x_square;
-    std::vector<float> m_kernel_weight;
-	int m_dim;
-
-	// svm_parameters
-	const int m_kernel_type;
-	const int m_degree;
-	const float m_gamma; // same as 1 / 2*sigma^2
-	const float m_coef0;
-	float m_kernel_norm;
-
-	static float dot(const svm_node_t* px, const svm_node_t* py);
-	static float dot(const svm_node_t* px, const svm_node_t* py, const std::vector<float>& weight);
-	static float matrix(const svm_node_t* px, const svm_node_t* py, const std::vector<float>& W, int dim);
-	float kernel_linear(int i, int j) const;
-	float kernel_poly(int i, int j) const;
-	float kernel_rbf(int i, int j) const;
-	float kernel_rbf_weight(int i, int j) const;
-	float kernel_rbf_w(int i, int j) const;
-	float kernel_sigmoid(int i, int j) const;
-	float kernel_precomputed(const int i, int j) const;
-};
-
-svm_model_t* svm_train(const svm_problem_t& prob, const svm_parameter_t& param);
-void svm_cross_validation(const svm_problem_t& prob, const svm_parameter_t& param, int num_fold, std::vector<float>* target);
-void svm_leave_one_in(const svm_problem_t& prob, const svm_parameter_t& param, int num_fold, std::vector<float>* errors);
-void svm_leave_one_out(const svm_problem_t& prob, const svm_parameter_t& param, int num_fold, std::vector<float>* errors);
-
-int	svm_save_model(const char* model_file_name, const svm_model_t& model);
-svm_model_t* svm_load_model(const char* model_file_name);
-
-int	svm_save_model_binary(const char* model_file_name, const svm_model_t& model);
-svm_model_t* svm_load_model_binary(const char* model_file_name);
-
-int	svm_get_svm_type(const svm_model_t& model);
-int svm_get_nr_class(const svm_model_t& model);
-std::vector<int> svm_get_labels(const svm_model_t& model);
-float svm_get_svr_probability(const svm_model_t& model);
-float svm_get_dual_objective_function(const svm_model_t& svm);
-
-void svm_predict_values(const svm_model_t& model, const svm_node_t& x, std::vector<float>* dec_values);
-float svm_predict(const svm_model_t& model, const svm_node_t& x);
-void svm_predict_votes(const svm_model_t& model, const svm_node_t& x, std::vector<float>* votes);
-float svm_predict_probability(const svm_model_t& model, const svm_node_t& x, std::vector<float>* prob_estimates);
-
-
-bool svm_check_probability_model(const svm_model_t& model);
+// if we trained a linear svm, it is possible to calculate just the weights and the offset
+// prediction is then y_hat = sign(X * w + b)
+void svm_get_linear_weights(const svm_model_t& model, std::vector<double>* w, double* b);
 
 #endif
--- src/svm.cpp
+++ src/svm.cpp
@@ -1,329 +1,74 @@
 #include "svm.h"
 
-#include <algorithm>
-#include <cmath>
-#include <cstdio>
+#include <cassert>
 
-#define M_TAU 1e-12
-#define M_INF HUGE_VAL
-
-template <class S, class T> inline void clone(T*& dst, S* src, int n)
+double kernel_linear(const std::vector<double>& v1, const std::vector<double>& v2)
 {
-	dst = new T[n];
-	memcpy((void *)dst,(void *)src,sizeof(T)*n);
+    double result = 0;
+    for(int i = 0; i< v1.size(); ++i) result += v1[i] * v2[i];
+    return result;
 }
 
-Cache::Cache(int l, long long size) : m_l(l), m_cache_mem_size(size)
+double svm_margin(const svm_model_t& model, const std::vector<double>& example) 
 {
-	m_head = new head_t[l]();
-	m_cache_mem_size /= sizeof(float);
-	m_cache_mem_size -= l * sizeof(head_t) / sizeof(float);
-	m_cache_mem_size = std::max(size, 2 * (long long) l);	// cache must be large enough for two columns
-	m_lru_head.next = m_lru_head.prev = &m_lru_head;
-}
+    double f = model.b;
 
-void Cache::lru_delete(head_t* h)
-{
-    // delete current position
-	h->prev->next = h->next;
-	h->next->prev = h->prev;
-}
-
-void Cache::lru_insert(head_t* h)
-{
-	// insert at tail-1
-	h->next = &m_lru_head;
-	h->prev = m_lru_head.prev;
-	h->prev->next = h->next->prev = h;
-}
-
-int Cache::get_data(const int index, std::vector<float>* data, int len)
-{
-	head_t* h = &m_head[index];
-	if(h->len) lru_delete(h);
-	int more = len - h->len;
-
-	if(more) {
-		// free old space
-		while(m_cache_mem_size < more) {
-			head_t* old = m_lru_head.next;
-			lru_delete(old);
-			m_cache_mem_size += old->len;
-			old->data = std::vector<float>(0);
-            old->len = 0;
-		}
-
-		h->data.resize(len);
-		m_cache_mem_size -= more;
-        std::swap(h->len, len);
-	}
-
-	lru_insert(h);
-	*data = h->data;
-	return len;
-}
-
-void Cache::swap_index(int i, int j)
-{
-	if(i == j) return;
-
-	if(m_head[i].len) lru_delete(&m_head[i]);
-	if(m_head[j].len) lru_delete(&m_head[j]);
-
-    std::swap(m_head[i].data, m_head[j].data);
-    std::swap(m_head[i].len, m_head[j].len);
-
-    //TODO: check if data goes out of scope before this
-	if(m_head[i].len) lru_insert(&m_head[i]);
-	if(m_head[j].len) lru_insert(&m_head[j]);
-
-	if(i > j) std::swap(i,j);
-	for(head_t* h = m_lru_head.next; h != &m_lru_head; h = h->next) {
-		if(h->len > i) {
-			if(h->len > j) std::swap(h->data[i], h->data[j]);
-			else {
-				// give up
-				lru_delete(h);
-				m_cache_mem_size += h->len;
-				h->data = std::vector<float>(0);
-                h->len = 0;
-			}
-		}
-	}
-}
-
-Kernel::Kernel(int l, svm_node_t* const* x, const svm_parameter_t& param)
-    : m_kernel_type(param.kernel_type), m_degree(param.degree),
-        m_gamma(param.gamma), m_coef0(param.coef0), m_kernel_weight(param.kernel_weight), m_kernel_norm(param.kernel_norm)
-{
-	switch(m_kernel_type) {
-		case LINEAR:
-			kernel_function = &Kernel::kernel_linear;
-			break;
-		case POLY:
-			kernel_function = &Kernel::kernel_poly;
-			break;
-		case RBF:
-			kernel_function = &Kernel::kernel_rbf;
-			break;
-		case RBFWEIGH:
-			kernel_function = &Kernel::kernel_rbf_weight;
-			break;
-		case RBFWMATRIX:
-			kernel_function = &Kernel::kernel_rbf_w;
-			break;
-		case SIGMOID:
-			kernel_function = &Kernel::kernel_sigmoid;
-			break;
-		case PRECOMPUTED:
-			kernel_function = &Kernel::kernel_precomputed;
-            break;
-	}
-
-	clone(m_x,x,l);
-
-	m_dim = param.kernel_dim;
-	if(!m_dim) {
-		while(m_x[0][m_dim].index != -1) m_dim++;
-	}
-
-	if(m_kernel_type == RBF) {
-		m_x_square = std::vector<float>(l, 0.f);
-		for(int i=0;i<l;++i) m_x_square[i] = dot(m_x[i], m_x[i]);
-	}
-	else if(m_kernel_type == RBFWEIGH) {
-		m_x_square = std::vector<float>(l, 0.f);
-		for(int i=0;i<l;++i) m_x_square[i] = dot(m_x[i], m_x[i], m_kernel_weight);
-	}
-	else m_x_square = std::vector<float>(0);
-}
-
-void Kernel::swap_index(int i, int j)
-{
-    std::swap(m_x[i], m_x[j]);
-	if(m_x_square.size()) std::swap(m_x_square[i], m_x_square[j]);
-}
-
-float Kernel::kernel_linear(int i, int j) const
-{
-	return m_kernel_norm*dot(m_x[i],m_x[j]);
-}
-
-float Kernel::kernel_poly(int i, int j) const
-{
-    return m_kernel_norm*pow(m_gamma*dot(m_x[i],m_x[j])+m_coef0, m_degree);
-}
-
-float Kernel::kernel_rbf(int i, int j) const
-{
-    return m_kernel_norm*exp(-m_gamma*(m_x_square[i]+m_x_square[j]-2*dot(m_x[i],m_x[j])));
-}
-
-float Kernel::kernel_rbf_weight(int i, int j) const
-{
-    float sum = 0;
-    const svm_node_t* px = m_x[i], *py = m_x[j];
-    while(px->index != -1 && py->index != -1) {
-        if(px->index == py->index) {
-            sum += (px->value - py->value) * (px->value - py->value) * m_kernel_weight[px->index-1];
-            ++px, ++py;
+    // if the linear kernel was used and w was computed and stored, 
+    // (i.e. the svm has fully finished training)
+    // the internal variable use_w will be set to true.
+    if(model.use_w) {
+        // we can speed this up a lot by using the computed weights
+        // we computed these during train(). This is significantly faster 
+        // than the version below
+        for(int i = 0; i < model.D; ++i) {
+            f += example[i] * model.w[i];
         }
-        else {
-            if(px->index > py->index) ++py;
-            else ++px;
+    } 
+    else {
+        for(int i = 0; i < model.N; ++i) {
+            f += model.alpha[i] * model.param.labels[i] * model.kernel(example, model.param.datum[i]);
         }
     }
-    return m_kernel_norm*exp(-m_gamma*sum);
+
+    return f;
 }
 
-float Kernel::kernel_rbf_w(int i, int j) const
+std::vector<double> svm_margins(const svm_model_t& model, const std::vector<std::vector<double> >& data)
 {
-	return m_kernel_norm*exp(-m_gamma*matrix(m_x[i], m_x[j], m_kernel_weight, m_dim));
+    // go over support vectors and accumulate the prediction.
+    int N = data.size();
+    std::vector<double> margins(N);
+    for(int i = 0; i < N; ++i) {
+        margins[i] = svm_margin(model, data[i]);
+    }
+    return margins;
 }
 
-float Kernel::kernel_sigmoid(int i, int j) const
+int svm_predict(const svm_model_t& model, const std::vector<double>& example)
 {
-	return tanh(m_gamma*dot(m_x[i],m_x[j])+m_coef0);
+    return svm_margin(model, example) > 0.f ? 1 : -1;
 }
 
-float Kernel::kernel_precomputed(int i, int j) const
+std::vector<int> svm_predict(const svm_model_t& model, const std::vector<std::vector<double> >& data)
 {
-	return m_x[i][(int)(m_x[j][0].value)].value;
+    std::vector<int> predictions(data.size());
+    std::vector<double> margins = svm_margins(model, data);
+    for(int i = 0; i < margins.size(); ++i) {
+        predictions[i] = margins[i] > 0.f ? 1 : -1;
+    }
+    return predictions;
 }
 
-float Kernel::dot(const svm_node_t* px, const svm_node_t* py)
+void svm_get_linear_weights(const svm_model_t& model, std::vector<double>* w, double* b)
 {
-	float sum = 0;
-	while(px->index != -1 && py->index != -1) {
-		if(px->index == py->index) {
-			sum += px->value * py->value;
-			++px, ++py;
-		}
-		else {
-			if(px->index > py->index) ++py;
-			else ++px;
-		}
-	}
-	return sum;
-}
-
-float Kernel::dot(const svm_node_t* px, const svm_node_t* py, const std::vector<float>& weight)
-{
-	float sum = 0.f;
-	while(px->index != -1 && py->index != -1) {
-		if(px->index == py->index) {
-			sum += px->value * py->value * weight[px->index];
-			++px, ++py;
-		}
-		else {
-			if(px->index > py->index) ++py;
-			else ++px;
-		}
-	}
-	return sum;
-}
-
-float Kernel::matrix(const svm_node_t* px, const svm_node_t* py, const std::vector<float>& W, int dim) {
-	float sum = 0.f;
-    std::vector<float> x_w(dim, 0.f);
-	for (int i=0; i<dim; ++i) {
-		for (int j=0; j<dim; ++j) {
-			x_w[i] += (px[j].value - py[j].value)*W[j*dim + i];
-		}
-	}
-	for (int i=0; i<dim; ++i) {
-		sum += (px[i].value - py[i].value)*x_w[i];
-	}
-	return sum;
-}
-
-
-float Kernel::k_function(const svm_node_t* x, const svm_node_t* y, const svm_parameter_t& param)
-{
-	switch(param.kernel_type) {
-		case LINEAR:
-			return dot(x,y);
-		case POLY:
-			return pow(param.gamma*dot(x,y)+param.coef0, param.degree);
-		case RBF: {
-			float sum = 0.f;
-			while(x->index != -1 && y->index !=-1) {
-				if(x->index == y->index) {
-					float d = x->value - y->value;
-					sum += d*d;
-					++x, ++y;
-				}
-				else {
-					if(x->index > y->index) {
-						sum += y->value * y->value;
-						++y;
-					}
-					else {
-						sum += x->value * x->value;
-						++x;
-					}
-				}
-			}
-
-			while(x->index != -1) {
-				sum += x->value * x->value;
-				++x;
-			}
-
-			while(y->index != -1) {
-				sum += y->value * y->value;
-				++y;
-			}
-			if(param.normalize_kernel) return param.kernel_norm*exp(-param.gamma*sum);
-			return exp(-param.gamma*sum);
-		}
-		case RBFWEIGH: {
-            float sum = 0.f;
-            while(x->index != -1 && y->index !=-1) {
-                if(x->index == y->index) {
-                    float d = x->value - y->value;
-                    sum += d*d*param.kernel_weight[x->index-1];
-                    ++x, ++y;
-                }
-                else {
-                    if(x->index > y->index) {
-                        sum += y->value * y->value * param.kernel_weight[y->index-1];
-                        ++y;
-                    }
-                    else {
-                        sum += x->value * x->value * param.kernel_weight[x->index-1];
-                        ++x;
-                    }
-                }
-            }
-
-            while(x->index != -1) {
-                sum += x->value * x->value * param.kernel_weight[x->index-1];
-                ++x;
-            }
-
-            while(y->index != -1) {
-                sum += y->value * y->value * param.kernel_weight[y->index-1];
-                ++y;
-            }
-            if(param.normalize_kernel) return param.kernel_norm*exp(-param.gamma*sum);
-            return exp(-param.gamma*sum);
+    assert(model.param.kernel_type == LINEAR);
+    *w = std::vector<double>(model.D);
+    for(int j = 0; j < model.D; ++j) {
+        double s = 0.f;
+        for(int i = 0; i < model.N; ++i) {
+            s += model.alpha[i] * model.param.labels[i] * model.param.datum[i][j];
         }
-		case RBFWMATRIX: {
-            int l = param.kernel_dim;
-            if(!l) {
-                while(x[l].index != -1) l++;
-            }
-            float sum = matrix(x, y, param.kernel_weight, l);
-            if(param.normalize_kernel) return param.kernel_norm*exp(-param.gamma*sum);
-            return exp(-param.gamma*sum);
-        }
-		case SIGMOID:
-			return tanh(param.gamma*dot(x,y)+param.coef0);
-		case PRECOMPUTED:  //x: test (validation), y: SV
-			return x[(int)(y->value)].value;
-		default:
-			return 0;  // Unreachable
-	}
+        (*w)[j] = s;
+    }
+    *b = model.b;
 }
-
